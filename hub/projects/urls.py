ebc52a81e143d34312431a52b190a02be0905b17
Ayan Islam <124712157+ayangithub00@users.noreply.github.com>
2026-04-28 14:59:43 +0530

    Update urls.py

--- hub/projects/urls.py
+++ hub/projects/urls.py
@@ -4,11 +4,7 @@ from . import views
 
 router = DefaultRouter()
 
-# When a ViewSet uses get_queryset() dynamically instead of a static
-# queryset = Model.objects.all() class attribute, Django REST Framework
-# cannot automatically figure out the basename for URL naming.
-# We must provide it manually with basename="project".
-# This generates URL names like: "project-list", "project-detail", "project-members"
+
 router.register("project", views.ProjectViewset, basename="project")
 
 urlpatterns = [
