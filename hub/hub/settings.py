c4c9dbe187ec61f6eab38c2132cda48d5805d70f
Ayan Islam <ayanislam78600@gmail.com>
2026-05-04 21:57:46 +0530

    configure database for render

--- hub/hub/settings.py
+++ hub/hub/settings.py
@@ -80,15 +80,13 @@ WSGI_APPLICATION = 'hub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'devhub_db',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:1234@localhost:5432/devhub_db',
+        conn_max_age=600
+    )
 }
 
 
