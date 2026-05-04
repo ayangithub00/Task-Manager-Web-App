2c4fc339b31b01191245c316e31590c0accd195c
Ayan Islam <ayanislam78600@gmail.com>
2026-05-04 21:55:10 +0530

    fix settings for deployment

--- hub/hub/settings.py
+++ hub/hub/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7t@k6qp_d%hh+dl@@w@+5ri99^g2_lal42_8%16f@@w3ixj-ej'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7t@k6qp_d%hh+dl@@w@+5ri99^g2_lal42_8%16f@@w3ixj-ej')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
